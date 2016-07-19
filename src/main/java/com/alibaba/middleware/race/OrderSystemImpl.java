f13907c7e041e8d5636ea25d45114adfb5a1a872
immortalCockroach <buptzhangyi@gmail.com>
2016-07-19 20:43:20 +0800

    去掉了hash扰动函数

--- src/main/java/com/alibaba/middleware/race/OrderSystemImpl.java
+++ src/main/java/com/alibaba/middleware/race/OrderSystemImpl.java
@@ -517,14 +517,15 @@ public class OrderSystemImpl implements OrderSystem {
 	}
 	
 	private int hashWithDistrub(Object k) {
-	        int h = 0;
-	        h ^= k.hashCode();
-
-	        // This function ensures that hashCodes that differ only by
-	        // constant multiples at each bit position have a bounded
-	        // number of collisions (approximately 8 at default load factor).
-	        h ^= (h >>> 20) ^ (h >>> 12);
-	        return h ^ (h >>> 7) ^ (h >>> 4);
+//	        int h = 0;
+//	        h ^= k.hashCode();
+//
+//	        // This function ensures that hashCodes that differ only by
+//	        // constant multiples at each bit position have a bounded
+//	        // number of collisions (approximately 8 at default load factor).
+//	        h ^= (h >>> 20) ^ (h >>> 12);
+//	        return h ^ (h >>> 7) ^ (h >>> 4);
+		return k.hashCode();
 	    
 	}
 	
