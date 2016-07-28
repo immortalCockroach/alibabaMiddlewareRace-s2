2cddd3ac2fada4cc75c59d768c9accec516a18e0
immortalCockroach <buptzhangyi@gmail.com>
2016-07-28 16:28:08 +0800

    修改buyer和good文件的切分为1024

--- src/main/java/com/alibaba/middleware/race/utils/CommonConstants.java
+++ src/main/java/com/alibaba/middleware/race/utils/CommonConstants.java
@@ -18,7 +18,7 @@ public class CommonConstants {
 	public static final int ORDER_SPLIT_SIZE = 4096;
 	
 	// 买家 商品文件切割份数
-	public static final int OTHER_SPLIT_SIZE = 128;
+	public static final int OTHER_SPLIT_SIZE = 1024;
 	
 	// 索引文件的每行record数目 整行的大小控制在400-800byte左右 此时读取和split性能较好，预估索引文件每行的单个记录在50bytes左右
 	public static final int INDEX_LINE_RECORDS = 12;
